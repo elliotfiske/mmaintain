1958a8459dad225b478f7f373fd0cb6cfcbbd544
Elliot Fiske <elliotfiske@gmail.com>
2025-04-10 09:33:40 -0700

    rm old import

--- elm-pkg-js-includes.js
+++ elm-pkg-js-includes.js
@@ -1,7 +1,6 @@
 
 const sizeObserver = require('./elm-pkg-js/element-size-observer.js')
 const dialogHelper = require('./elm-pkg-js/dialog.js')
-const preventDoubleClick = require('./elm-pkg-js/prevent-doubleclick.js')
 
 exports.init = async function init(app) {
     await sizeObserver.init(app)
